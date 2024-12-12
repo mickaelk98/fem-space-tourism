589375c451333c1dc2fe875a388f9e75c7f01277
mickael keita <keitamickael@gmail.com>
2024-12-12 15:13:36 +0100

    add a responsive home page

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,23 @@ function App() {
   return (
     <div className={styles.container}>
       <Header />
-      <main></main>
+      <main className={styles.main}>
+        <div className={styles.textBlock}>
+          <h1>
+            So, you want to travel to <br />
+          </h1>
+          <span>Space</span>
+          <p>
+            Let’s face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we’ll give you a truly out of this
+            world experience!
+          </p>
+        </div>
+        <div className={styles.cta}>
+          <button>Explore</button>
+        </div>
+      </main>
     </div>
   );
 }
